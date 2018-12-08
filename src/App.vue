28528e142b574abeecdc250872290e990259d4a7
tsu2626 <dmwtpdmwtp24@gmail.com>
2018-12-08 23:51:45 +0900

    isLogin_page

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,26 @@
 <template>
   <div id="app">
-    <img src="logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <Home v-if="!isLogin"></Home>
+    <Editor v-if="isLogin"></Editor>
   </div>
 </template>
 
 <script>
+import Home from './components/Home';
+import Editor from './components/Editor';
+
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+      isLogin: true
+    };
+  },
+  components: {
+    Home: Home,
+    Editor: Editor
   }
-}
+};
 </script>
 
 <style lang="scss">
---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="home">
+    <h1>{{ msg }}</h1>
+    <button @click="googleLogin">アカウントでログイン</button>
+  </div>
+</template>
+<script>
+  export default {
+    name: "home",
+    data () {
+      return {
+        msg:"Welcome To MyMarkdown"
+      };
+    }
+  }
+</script>

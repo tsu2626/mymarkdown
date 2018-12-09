c65a7ad44053946f92f864b68b8610b38095ab3b
tsu2626 <dmwtpdmwtp24@gmail.com>
2018-12-09 22:56:25 +0900

    preview_markdown

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -3,20 +3,47 @@
     <h1>エディター画面</h1>
     <span>{{ user.displayName }}</span>
     <button @click="logout">ログアウト</button>
+    <div class="editorWrapper">
+      <textarea class="markdown" v-model="markdown"></textarea>
+      <dir class="preview" v-html="preview()"></dir>
+    </div>
   </div>
 </template>
 
 <script>
+  import marked from "marked";
   export default {
     name: "editor",
     props: ["user"],
     data () {
-      return　{};
+      return　{
+        markdown: "" //markdownの初期dataは空
+      };
     },
     methods: {
       logout: function() {
         firebase.auth().signOut();
+      },
+      preview: function() { //prebview関数でmarkdownの値を格納したmarkedへreturn
+        return marked(this.markdown);
       }
     }
   };
 </script>
+
+<style lang="scss" scoped>
+.editorWrapper {
+  display: flex;
+}
+
+.markdown {
+  width: 50%;
+  height: 50%;
+}
+
+.preview {
+  width: 50%;
+  text-align: center;
+}
+  
+</style>

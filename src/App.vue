e935370fa9e0d46bac4a9f75018660432deb8c53
tsu2626 <dmwtpdmwtp24@gmail.com>
2018-12-09 22:05:00 +0900

    logout&indicate_userName

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <Home v-if="!isLogin"></Home>
-    <Editor v-if="isLogin"></Editor>
+    <Editor v-if="isLogin" :user="userData"></Editor>
   </div>
 </template>
 
@@ -13,22 +13,25 @@ export default {
   name: 'app',
   data () {
     return {
-      isLogin: false
+      isLogin: false,
+      userData: null
     };
-  },
-  components: {
-    Home: Home,
-    Editor: Editor
   },
   created: function() {
     firebase.auth().onAuthStateChanged(user => {
       console.log(user);
       if(user) {
         this.isLogin = true;
+        this.userData = user;
       }else{
         this.isLogin = false;
+        this.userData = null;
       };
     });
+  },
+  components: {
+    Home: Home,
+    Editor: Editor
   },
 };
 </script>

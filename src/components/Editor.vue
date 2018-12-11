7a317da9a8bb1d4e2614eacb7f61f95ff5f0cd1e
tsu2626 <dmwtpdmwtp24@gmail.com>
2018-12-12 01:06:52 +0900

    create_delBtn

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -3,10 +3,16 @@
     <h1>エディター画面</h1>
     <span>{{ user.displayName }}</span>
     <button @click="logout">ログアウト</button>
-    <div class="editorWrapper">
-      <textarea class="markdown" v-model="markdown"></textarea>
-      <dir class="preview" v-html="preview()"></dir>
+    <div class="memoListWrapper">
+      <div class="memoList" v-for="(memo, index) in memos" :key="memo, index"
+        @click="selectMemo(index)" :data-selected="index == selectedIndex">
+        <p class="memoTitle">{{ displayTitle(memo.markdown) }}</p>
+      </div>
+      <button class="addMemoBtn" @click="addMemo">追加</button>
+      <button class="deleteMemoBtn" v-if="memos.length>1" @click="deleteMemo">削除</button>
     </div>
+    <textarea class="markdown" v-model="memos[selectedIndex].markdown"></textarea>
+    <dir class="preview" v-html="preview()"></dir>
   </div>
 </template>
 
@@ -17,33 +23,81 @@
     props: ["user"],
     data () {
       return　{
-        markdown: "" //markdownの初期dataは空
+        memos: [
+          {
+            markdown: "" //markdownの初期dataは空          
+          }
+        ],
+        selectedIndex: 0
       };
     },
     methods: {
       logout: function() {
         firebase.auth().signOut();
       },
+      addMemo: function() {
+        this.memos.push({
+          markdown: "無題のメモ"
+        });
+      },
+      deleteMemo: function() {
+        this.memos.splice(this.selectedIndex, 1);
+        if (this.selectedIndex > 0) {
+          this.selectedIndex--;
+        }
+      },
+      selectMemo: function(index) {
+        this.selectedIndex = index;
+      },
       preview: function() { //prebview関数でmarkdownの値を格納したmarkedへreturn
-        return marked(this.markdown);
+        return marked(this.memos[this.selectedIndex].markdown);
+      },
+      displayTitle: function(text) {
+        return text.split(/\n/)[0];
       }
     }
   };
 </script>
 
 <style lang="scss" scoped>
-.editorWrapper {
-  display: flex;
+// .editorWrapper {
+//   display: flex;
+// }
+
+.memoList {
+  padding: 10px;
+  box-sizing: border-box;
+  text-align: left;
+  border-bottom: 1px solid #000;
+  // &:nth-child(even) {
+  //   background-color: #ccc;
+  // }
+  &[data-selected="true"] {
+    background-color: #ccf;
+  }
+}
+
+.memoTitle {
+  height: 1.5rem;
+  margin: 0;
+  white-space: nowrap;
+  overflow: hidden;
+}
+
+.addMemoBtn {
+  margin-top: 20px;
 }
 
 .markdown {
-  width: 50%;
-  height: 50%;
+  float: left;
+  width: 40%;
+  height: 500px;
 }
 
 .preview {
-  width: 50%;
-  text-align: center;
+  float: left;
+  width: 40%;
+  text-align: left;
 }
   
 </style>

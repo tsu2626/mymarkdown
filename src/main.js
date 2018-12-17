246e11111ad611232bb1eccd257675434f0eca52
tsu2626 <dmwtpdmwtp24@gmail.com>
2018-12-18 08:57:29 +0900

    add_vuetify

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import '@mdi/font/css/materialdesignicons.css'
+
+Vue.use(Vuetify);
+Vue.config.productionTip = false;
 
 new Vue({
   el: '#app',
